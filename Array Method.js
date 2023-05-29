831ec8a1d0ded1dcb8a4720a6423f3eb0a4ff644
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-29 23:07:21 +0530

    Add files via upload

---
+++ Array Method.js
@@ -0,0 +1,22 @@
+let num = [1,2,3,4]
+let b = num.toString()
+console.log(b)
+let c = num.join("_")
+console.log(c,typeof(c))
+
+let d = num.pop()
+console.log(d)
+
+
+let e = num.push(34)
+console.log(num)
+
+let f = num.shift()
+console.log(f)
+console.log(num)
+
+
+let g = num.unshift()
+console.log(g) 
+
+console.log(num)
