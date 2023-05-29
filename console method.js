c8cd7d8713b3dcecd048207068a03a758d1fe1b0
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-29 23:08:41 +0530

    Add files via upload

---
+++ console method.js
@@ -0,0 +1,13 @@
+console.log("Hello")
+console.error("Hye it is Ayush")
+
+
+// let obj = (a : 1 , b : 3)
+// console.table(obj)
+
+
+
+console.info("Hey this is Worning")
+
+console.time("Heloo")
+console.assert("hello")
