e5f92de8eef8416574d86e8b8b1f779cb583f4ad
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-29 23:07:40 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+let marks = [91,52,73,84]
+console.log(marks)
+console.log(marks[1])
+console.log(marks[3])
+console.log(marks[0])
+console.log("The Number of Marks is", marks.length)
+marks[4] = 89
+
+console.log(marks[4])
+console.log(typeof marks)
