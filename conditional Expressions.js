3923b55410eab33d08392d3a40388c489963b783
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-29 23:08:19 +0530

    Add files via upload

---
+++ conditional Expressions.js
@@ -0,0 +1,28 @@
+// let a = prompt("Hey whats your age?");
+
+// a = number.parseInt(a) // converting the sting in number 
+
+// if(a<0){
+//     alert("This is a valid age");
+// }
+
+// else if(a<9){
+//     alert("you are a kid and you cannot even think of driving")
+// }
+// else if(a<18 && a>=9){
+//     alert("you are a kid and you cannot even think of driving")
+// }
+// else{
+//     alert("This is an invalid age");
+// }
+
+// console.log("Done")
+
+
+
+//Explore switch statement 
+
+
+// ternari operater 
+a = 19
+console.log("you can", (a<18? "not drive" : "drive"))
