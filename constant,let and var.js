1c4fb99b8bce6c397fd8ba3d764bab503f55c41c
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-30 21:29:24 +0530

    Add files via upload

---
+++ constant,let and var.js
@@ -0,0 +1,15 @@
+console.log("JavaScript Tutorial 3: var, let and const")
+var a = 23
+var b = "Harry"
+var c = null
+var d = undefined
+
+console.log(b)
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(b)
+
+const author = "Harry"
+let a = "hello"
