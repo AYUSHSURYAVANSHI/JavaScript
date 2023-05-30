283d0a01b73922ad7e145a342eb1a1b191c0491e
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-30 21:31:23 +0530

    Add files via upload

---
+++ For loop in javascript.js
@@ -0,0 +1,40 @@
+// // program to add first natural number 
+// let sum = 0
+// let n = prompt("Enter the value of n ")
+// n = Number.parseInt(n)
+// for(let i =0; i<100; i++) {
+//     sum = sum + (i+1)
+
+// }
+// console.log("Sum of first " + n + "natural number is" + sum)
+
+// let sum = 0
+// let n = prompt("Enter the value of n ")
+// n = Number.parseInt(n)
+// for(let i =0; i<100; i++) {
+//     sum = sum * (i+1)
+    
+// }
+// console.log("Sum of first " + n + "natural number is" + sum)
+consol.log(i) 
+
+
+// for in loop
+
+let obj = {
+    harry : 90,
+    shubh : 45,
+    shivika : 56,
+    ritika : 57,
+    shiv : 23
+}
+for (let a in obj){
+    console.log("Marks of " + a + "are" + obj[a] )
+}
+
+// for of loop 
+for (let b in obj){
+    console.log(b)
+}
+
+
