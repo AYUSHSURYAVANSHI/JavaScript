776e201b97769885f5da75cb1e1b4f78d5859126
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-30 21:29:52 +0530

    Add files via upload

---
+++ Element only Navigation.js
@@ -0,0 +1,11 @@
+const changeBgRed = ()=>{
+    document.body.firstElementChild.getElementsByClassName.background = "red"
+}
+
+
+let b = document.body
+console.log("First child of b is: ",b.firstChild)
+console.log("First Element child of b is: ",b.firstElementChild)
+
+
+
