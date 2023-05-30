5442a7ffd0570da58f3ef938bddeaf7c8286ec51
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-30 21:30:15 +0530

    Add files via upload

---
+++ Expressions and Operators.js
@@ -0,0 +1,57 @@
+// 77;
+// "Harray"
+// Arthmatic Operator
+console.log("Operators in js")
+let a = 45;
+let b = 4;
+console.log("a + b = ",a+b)
+console.log("a - b = ",a-b)
+console.log("a * b = ",a*b)
+console.log("a / b = ",a/b)
+console.log("a ** b = ",a**b)
+console.log("a % b = ",a%b)
+console.log("a++ = ",a++)
+console.log("a-- = ",a--)
+console.log("--a = ",--a)
+console.log("++a = ",++a) 
+console.log("++a = ",++a) 
+console.log("*a = ",a)  
+
+// Assignment Operator
+let ass = 1
+ass += 5
+console.log(ass)
+
+
+
+// comarition Operator 
+
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2",comp1 == comp2)
+console.log("comp1 === comp2",comp1 === comp2)
+console.log("comp1 != comp2",comp1 != comp2)
+
+
+
+
+// logical Operator 
+
+let x = 4;
+let y = 5;
+console.log(x<y && x==4)
+console.log(x<y || x==5)
+console.log(!false)
+console.log(!true)
+
+// comments 
+// single line 
+// This is a single line comment 
+
+// Multiline comment 
+/*This is 
+a multi line code 
+line comment 
+*/
+
+
