ef23bbdfb100496e46b395368ed792c1e449e295
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-30 21:30:39 +0530

    Add files via upload

---
+++ Expretions and condition.js
@@ -0,0 +1,65 @@
+// // chapter 2 
+// // practice set 
+// // let age = 67
+// let age = prompt("what is your age?")  
+// if(age>10 && age<20){
+//     console.log("Your age lies between 10 and 20 ")
+// }
+// else{
+//     console.log("Your age lies between 10 and 20 ")
+// }
+
+// let age = prompt("what is your age?")
+// age = Number.parseInt(age)
+// switch(age){
+//     case '12':
+//         console.log("Your age is 12")
+//         break
+//     case '13':
+//         console.log("Your age is 12")
+//         break
+//     case '14':
+//         console.log("Your age is 12")
+//         break
+//     case '15':
+//         console.log("Your age is 12")
+//         break
+//     default:
+//         console.log("Your age is not special")
+
+// }
+
+
+// let num = prompt("what is your age?")
+// num = Number.parseInt(num)
+// if(num %2 ==0 && num % 3 ==0){
+//     console.log("Your number is divisible by 2 and 3")
+// }
+
+// else{
+//     console.log("Your number is  not divisible by 2 and 3")
+    
+// }
+
+
+
+// let num = prompt("what is your age?")
+// num = Number.parseInt(num)
+// if(num %2 ==0 && num % 3 ==0){
+//     console.log("Your number is divisible by 2 and 3")
+// }
+
+// else{
+//     console.log("Your number is  not divisible by 2 and 3")
+    
+// }
+
+
+let age = 19
+let a = age > 18 ? "You can drive" : "you cannot drive"
+console.log(a)
+
+
+
+
+
