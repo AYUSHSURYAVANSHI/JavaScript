e5f543a53222e38c78cde67a63ba3ea05c09a944
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-31 23:33:30 +0530

    Add files via upload

---
+++ innerHtml,OuterHtml,And Properties.js
@@ -0,0 +1,9 @@
+let x = document.getElementsByTagName('span')[0]
+console.log(x)
+let y = document.getElementsByTagName('span')[0]
+console.dir(x)
+console.log(document.body.firstChild.nodeName)
+console.log(document.body.firstElementChild.nodeName)
+ 
+
+
