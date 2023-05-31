c6bca15ef139c4b8a7cb84219b9655f31fa5dadc
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-31 23:32:39 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,27 @@
+function onePlusAvg(x,y){
+    console.log("Done")
+    return 1 + (x + y)/2
+}
+let a = 1
+let b = 2
+let c = 3
+
+
+const sum = (p,q)=>{
+    return p + q
+}
+
+console.log(sum(9,7))
+
+
+
+
+console.log("Average of a and b is ",1 +onePlusAvg(a,b))
+console.log("Average of b and c is ",1 +onePlusAvg(b,c))
+console.log("Average of a and c is ",1 +onePlusAvg(a,c))
+
+
+
+console.log("Average of a and b is ",1 +(a + b)/2)
+console.log("Average of b and c is ",1 +(b + c)/2)
+console.log("Average of a and c is ",1 +(c + a)/2)
