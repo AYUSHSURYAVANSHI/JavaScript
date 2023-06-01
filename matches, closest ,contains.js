486e07ba62e3699d2566c9090ec8bf5c4439d749
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-01 22:21:00 +0530

    Add files via upload

---
+++ matches, closest ,contains.js
@@ -0,0 +1,12 @@
+let id1 = document.getElementById("id1")
+let id2 = document.getElementById("id2")
+console.log(id1)
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+console.log(sp1.closest("#sp1"))
+console.log(id1.contains(sp1))
+console.log(sp1.contains(sp1))
+console.log(sp1.contains(sp1))
+console.log(sp1.contains(id1))
+
+
