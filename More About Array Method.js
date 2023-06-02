ec2b5ff260ca785b0710f72c2148a536cc33b2ba
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-02 12:46:30 +0530

    Add files via upload

---
+++ More About Array Method.js
@@ -0,0 +1,22 @@
+let num = [45,66,33,22,13,12,56]
+let num_more = [45,66,33,22,13,12,56]
+let num_about = [45,66,33,22,13,12,56]
+console.log(num)
+
+let a = delete num[0]
+console.log(a)
+
+console.log(num)
+
+
+let bus = num.concat(num_more,num)
+console.log(bus)
+
+
+let b = num.sort()
+console.log(b)
+
+// num.reverse
+
+
+// num.slice [position, numbers , value ]
