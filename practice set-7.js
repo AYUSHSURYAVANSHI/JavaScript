cba515210529f811c89cbf7c6f1dcd8c100b0479
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-02 12:50:23 +0530

    Add files via upload

---
+++ practice set-7.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background ="red";
+    
+})
