96afdf42517e1f7ceb64c15967b71577ec32d0cc
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-02 12:48:08 +0530

    Add files via upload

---
+++ Practice Set-1.js
@@ -0,0 +1,32 @@
+let a = "Harry"
+let b = 5
+console.log(a + b)
+
+
+console.log(typeof (a+b))
+
+const c = {
+    name : "Harry",
+    section : 1,
+    isPrincipal : false
+}
+d = "Harry"
+
+
+a1['friend'] = "Shubham" // a1 is refrence tool  
+a1['name'] = "Lovish"
+console.log(a1)  
+
+const dict = {
+
+    name : "Ayush",
+    city : "Chhindwara",
+    state : "Madhya Pradesh",
+    collage : "SATI Vidisha"
+}
+
+console.log(dict['city'])
+console.log(dict.city)
+
+
+
