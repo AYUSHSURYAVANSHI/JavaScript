2407fe3ba4aa81a8489d26b2e02e88bb042215db
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-03 22:57:21 +0530

    Add files via upload

---
+++ searching the DOM.js
@@ -0,0 +1,11 @@
+let ctitle = document.getElementsByClassName("card-title")[0]
+ctitle.style.color = "blue"
+
+
+let = ctitles = document.querySelector(".card-title")
+console.log(ctitle)
+
+
+ctitle[0].style.color = "blue"
+ctitle[1].style.color = "red"
+ctitle[2].style.color = "green"
