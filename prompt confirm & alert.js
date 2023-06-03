4ebad678ff74107d0fbe5c40cc83609805fa3be5
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-03 22:56:35 +0530

    Add files via upload

---
+++ prompt confirm & alert.js
@@ -0,0 +1,9 @@
+// alert("Hello your script works!")
+// let a = prompt("Enter the value of a")
+let write = confirm("Do you want it to the page")
+if(write){
+  document.write(a)
+}
+else{
+    document.write("Please Allow me to write")
+}
