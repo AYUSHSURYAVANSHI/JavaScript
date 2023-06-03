8e169e852aa6bf66c04ddcd58d5125e56d360ee5
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-03 22:58:12 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,18 @@
+let name = "Hello"
+console.log(name.length)
+
+let friend = 'prakash'
+console.log(friend[0])
+ 
+
+
+let boy1 = "Pramode"
+let boy2 = "Nikhil"
+
+let sentence = '${boy1} is a friend of ${boy2}'
+console.log(sentence)
+
+// Escape sequence 
+
+let fruit = "Banana"
+console.log(fruit)
