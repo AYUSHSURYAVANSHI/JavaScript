af6a8877cfe87d056cba96c86eec1f9fdc2ef32a
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-03 22:57:43 +0530

    Add files via upload

---
+++ Strings Methods.js
@@ -0,0 +1,17 @@
+let name = "Ayush"
+console.log(name.length)
+let friend = "Monika"
+console.log(name.concat("is a friend of ",friend, "Ok"))
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.replace("Ayu","Per"))
+console.log(name.slice(2,4))
+
+
+let friend2 = "    Prerna     "
+
+let fr = "Raman" + "Shivika" + "Harry"
+
+console.log(fr)
+console.log(friend2.trim())
+
