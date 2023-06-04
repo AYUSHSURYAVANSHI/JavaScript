e45ac3c81f56a431c8b95e21413d299e474c4490
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-04 23:00:30 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+console.log("THis is rapl contains code for the Ultimate javascript")
+let a = 67
+console.log(a)
+a = "harry"
+console.log(a)
+
+let b = 96 
+console.log(b)
+b = "harry"
+console.log(b)
+let c = 67
+console.log(c)
+
+
+
+
+
+
