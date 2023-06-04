5dede8f6d666c4f42ca6ccde86978876a21a8f31
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-04 22:57:04 +0530

    Add files via upload

---
+++ Table Navigation.js
@@ -0,0 +1,12 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.row[1].rowIndex)
+
+
+
+
