6af93d7bbaf38b67172623e089c11e7dd4eca6a4
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-04 23:01:39 +0530

    Add files via upload

---
+++ while loop.js
@@ -0,0 +1,46 @@
+let n = prompt("Enter the value of n ")
+n = Number.parseInt(n)
+
+let i = 0
+while(i < n ){
+    console.log(i)
+    i++;
+}
+
+// do while loop
+
+let p = prompt("Enter the value of n ")
+p = Number.parseInt(p)
+let j = 0
+do{
+
+    console.log(i)
+    i++;
+} while(j < n )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
