0012f26a38ce51a1062a30934f27ce553e965c52
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-05 20:25:20 +0530

    Add files via upload

---
+++ Html Insertion Method.js
@@ -0,0 +1,22 @@
+let a = document.getElementsByTagName('div')[0]
+a.innerHTML= a.innerHTML + '<h1>Hello World!</h1>;'
+
+// let a = document.getElementsByTagName('div')[0]
+// a.innerHTML= a.innerHTML + '<h1>Hello World!</h1>;'
+
+let div = document.createElement('div');
+a.innerHTML= a.innerHTML + '<h1>Hello World!</h1>';
+// a.append(div);
+// a.prepend(div);
+// a.before(div);
+// a.after(div);
+a.replaceWith(div);
+
+
+
+
+
+
+
+
+
