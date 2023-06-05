a9758576e90214165ae8b6e298cae2cd3001c033
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-05 20:21:15 +0530

    Add files via upload

---
+++ Callback Helln & Pyramid of Doom.js
@@ -0,0 +1,86 @@
+
+// callback
+function loadScript(url, callback){
+    var script = document.createElement("script");
+    script.src = src; 
+
+    script.onload = funtion()
+       console.log("Loaded script with SRC: " + src)
+       callback(new Error("Src got some Error"))
+    
+    document.body.appendChild(script);
+}
+
+function GoodMorning(error,src){
+    if(error){
+        console.log(error)
+        sendEmergencyMessageToCeo();
+        return 
+    }
+    alert('Good Morning' + src)
+}
+
+
+function GoodMorning(){
+    alert('Hello World Good Morning')
+}
+// loadScript(" <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+
+// Pyramid of DOMs  it is also Called Call Back Hells
+
+
+function GoodMorning(error,src){
+    if(error){
+        console.log(error)
+        sendEmergencyMessageToCeo();
+        return 
+    }
+    alert('Good Morning' + src)
+}
+
+
+function GoodMorning(){
+    alert('Hello World Good Morning')
+}
+// loadScript(" <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+
+// Pyramid of DOMs  it is also Called Call Back Hells
+
+
+function GoodMorning(error,src){
+    if(error){
+        console.log(error)
+        sendEmergencyMessageToCeo();
+        return 
+    }
+    alert('Good Morning' + src)
+}
+
+
+function GoodMorning(){
+    alert('Hello World Good Morning')
+}
+// loadScript(" <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+
+// Pyramid of DOMs  it is also Called Call Back Hells
+  
+
+
+function GoodMorning(error,src){
+    if(error){
+        console.log(error)
+        sendEmergencyMessageToCeo();
+        return 
+    }
+    alert('Good Morning' + src)
+}
+
+
+function GoodMorning(){
+    alert('Hello World Good Morning')
+}
+// loadScript(" <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
+
+// Pyramid of DOMs  it is also Called Call Back Hells
+
+
