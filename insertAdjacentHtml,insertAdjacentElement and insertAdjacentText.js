352bf626807c36e7aa7af7943b9ff159cd6ff42c
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-07 20:38:23 +0530

    Add files via upload

---
+++ insertAdjacentHtml,insertAdjacentElement and insertAdjacentText.js
@@ -0,0 +1,6 @@
+first.insertAdjacentHTML('beforeend','<div class="test">beforeend</div>');
+first.insertAdjacentHTML('beforbegin','<div class="test">beforbegin</div>');
+first.insertAdjacentHTML('afterbegin','<div class="test">afterbegin</div>');
+first.insertAdjacentHTML('afterend','<div class="test">afterend</div>');
+first.remove()
+
