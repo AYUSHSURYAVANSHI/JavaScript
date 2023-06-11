0fdebdfb87a1a3cfea50e89d44ba2d17e3fd52f5
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-11 23:28:17 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+
+function MulTable(){
+    for(i=1; i<=10; i++){
+        document.write('2 x ${i} = ${2*i}');
+        document.write("<br>")
+    }
+}
+
+
+MulTable();
+document.write("Hello <br>")
+
+MulTable();
