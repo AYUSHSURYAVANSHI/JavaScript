b0a7e3a2f9256636ea74b36864f6855aae184281
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-11 23:29:05 +0530

    Add files via upload

---
+++ Global Variable vs Local Variable.js
@@ -0,0 +1,22 @@
+let car = "audi"
+
+function add(){
+
+    let result = 33;
+
+
+}
+
+function sub(){
+    let result = 33;
+    console.log(result)
+}
+
+function sub(){
+    let result = 33;
+    console.log(result)
+}
+
+console.log(car)
+
+add();
