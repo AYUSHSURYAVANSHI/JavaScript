15813a100899a1ce124c5fc623a8a0679bb0d361
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-12 20:57:56 +0530

    Add files via upload

---
+++ Math Object.js
@@ -0,0 +1,15 @@
+console.log(Math.PI)
+console.log(Math.E)
+
+
+var x = 4.7;
+console.log(x)
+
+console.log(Math.ceil(4.6))
+
+
+console.log(Math.floor(3,5))
+
+
+console.log(Math.min(5,7))
+console.log(Math.max(5,7))
