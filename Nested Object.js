ff19145cc193a175b0f1c06dc414798a4ee04a08
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-14 20:11:42 +0530

    Add files via upload

---
+++ Nested Object.js
@@ -0,0 +1,20 @@
+var user = {
+    id : 101,
+    email: "surywanshiayssh@gmail.com"
+    // personalInfo: {
+        // name : "abc",
+        // Address: {
+        //     Styeet: "dsgsgs",
+        //     city: "Delhi",
+        //     country : "India"
+        // }
+
+    }
+
+// };
+
+
+console.log(user.personalInfo.Address.country);
+
+
+
