b4237844c70e5b754b61c204bf886fc3420671a6
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-14 20:13:40 +0530

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,17 @@
+let person = {
+    'first Name': 'vishwajeet',
+    'last Name': 'kumar'
+ };
+
+ console.log(person['last Name'])
+ person.age = 25;
+ person.firstName = 'Jeet';
+ console.log(person)
+
+
+
+ delete person.lastName;
+
+ console.log('height' in person);
+
+ 
