9be24b6d3600789966a2fad6fecffe88ec2560fc
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-14 20:13:06 +0530

    Add files via upload

---
+++ Object Prototype.js
@@ -0,0 +1,12 @@
+student1.prototype.nationality = "Indian ";
+
+
+
+student1.prototype.name = function(){
+    return this.firstName + " " + this.lastName;
+}
+
+
+var student1 = new student1("AyushSuryavanshi","Kumar")
+
+console.log(Student1)
