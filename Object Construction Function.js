de489e3c1765093a73a2c895758293509873bd3c
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-14 20:12:07 +0530

    Add files via upload

---
+++ Object Construction Function.js
@@ -0,0 +1,12 @@
+var student1 = {
+    firstName : "Vishwajeet",
+    lastName : "kumar",
+    age : 25,
+    class: 3
+}
+var student1 = {
+    firstName : "ajeet",
+    lastName : "kumar",
+    age : 26,
+    class: 3
+}
