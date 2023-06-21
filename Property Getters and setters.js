fc48c97c110447d741648319605accd9b7310681
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-21 21:54:22 +0530

    Add files via upload

---
+++ Property Getters and setters.js
@@ -0,0 +1,22 @@
+// var person ={
+
+// name : "Ayush Suryavanshi",
+// person : 26,
+
+
+// getname: function(){
+//     return this.name.toUpperCase();
+// }
+
+// };
+
+
+person.name = "ajit"
+
+console.log(person.getname());
+
+
+// set setname(n){
+//     this.name = n.toUpperCase();
+
+// }
