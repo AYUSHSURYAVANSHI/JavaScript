a6b9bd5f5a3132191dd7e0e6e592785ae7908550
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-21 21:51:47 +0530

    Add files via upload

---
+++ Parameter And Argument.js
@@ -0,0 +1,22 @@
+
+function MulTable(num){
+    for(i=1; i<=10; i++){
+        document.write('${num} x ${i} = ${2*i}');
+        document.write("<br>")
+    }
+    documnet.write("<br><br>");
+}
+
+
+MulTable(4);
+
+document.write("Hello <br>")
+
+MulTable(2);
+
+
+
+function add(num1,num2){
+    documnet.write(num1 + num2);
+}
+
