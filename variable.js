1a07fb600fa8048600fb836fe376b8395e4a1b21
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-06-23 00:34:23 +0530

    Add files via upload

--- variable.js
+++ variable.js
@@ -1,18 +1,6 @@
-console.log("THis is rapl contains code for the Ultimate javascript")
-let a = 67
-console.log(a)
-a = "harry"
-console.log(a)
-
-let b = 96 
-console.log(b)
-b = "harry"
-console.log(b)
-let c = 67
-console.log(c)
+var a;
+a = 4;
+alert(a)
 
 
-
-
-
-
+// this will popup 4
